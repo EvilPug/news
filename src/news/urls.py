06fdddd3db6eb05c0d167566d26918e8258c8b29
unknown <3608690@mail.ru>
2019-06-11 12:15:27 +0300

    Постраничный вывод новостей. Обновлены requirements.

--- src/news/urls.py
+++ src/news/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = 'news'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.NewsList.as_view(), name='index'),
     path('favorite/', views.favorite, name='favorite'),
-    path('add_label/', views.add_label, name='add_label'),
+    path('add_label/', views.NewsAddLabel.as_view(), name='add_label'),
     path('add_favorite/', views.add_favorite, name='add_favorite'),
     path('update_news/', views.update_news, name='update_news'),
     path('recommendations/', views.recommendations, name='recommendations'),
